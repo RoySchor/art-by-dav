b2e8be2d76c7c02f871d5d2bdf30fdb97de80153
Roy Schor <rps5836@psu.edu>
2025-12-14 00:29:54 -0500

    attempting another fix to url issue

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,11 @@ import "./styles.css";
 
 if (/github\.io$/i.test(window.location.hostname)) {
   const p = new URLSearchParams(window.location.search).get("p");
-  if (p) window.history.replaceState({}, "", p);
+  if (p) {
+    const base = (import.meta.env.BASE_URL || "/").replace(/\/+$/, ""); // "/art-by-dav"
+    const rel = p.startsWith("/") ? p : `/${p}`; // "/artwork"
+    window.history.replaceState({}, "", `${base}${rel}`); // "/art-by-dav/artwork"
+  }
 }
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
