df2f74a54075e892065db1b79fc21bea12db84d4
Roy Schor <rps5836@psu.edu>
2025-12-14 01:11:50 -0500

    made scrolling too responsive...

--- src/pages/artwork/Coverflow/Coverflow.tsx
+++ src/pages/artwork/Coverflow/Coverflow.tsx
@@ -31,10 +31,10 @@ const Coverflow: React.FC<Props> = ({ items, index, onIndexChange, onOpen }) => 
   const sideScale = 0.9;
   const depthPerStep = isMobile ? 60 : 80;
   const DRAG_THRESHOLD_PX = isMobile ? 10 : 42;
-  const STEP_COOLDOWN_MS = isMobile ? 20 : 160;
+  const STEP_COOLDOWN_MS = isMobile ? 25 : 160;
 
   const wheelAcc = useRef(0);
-  const WHEEL_THRESHOLD = isMobile ? 20 : 100;
+  const WHEEL_THRESHOLD = isMobile ? 40 : 100;
   const didDrag = useRef(false);
 
   const handleWheel = useCallback(
