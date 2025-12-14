5bcf7b124c638a68d17e9b897af19e97232c3e52
Roy Schor <rps5836@psu.edu>
2025-12-14 00:25:25 -0500

    fixing bad loading page

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 
-if (!/github\.io$/i.test(window.location.hostname)) {
-  const url = new URL(window.location.href);
-  if (url.searchParams.has("p")) {
-    window.history.replaceState({}, "", import.meta.env.BASE_URL || "/");
-  }
+if (/github\.io$/i.test(window.location.hostname)) {
+  const p = new URLSearchParams(window.location.search).get("p");
+  if (p) window.history.replaceState({}, "", p);
 }
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
